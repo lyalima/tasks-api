18d8fa1c872d5ed1ac358a5c6fc3114c8092f497
Lya Lima <lyalima0328@gmail.com>
2024-11-13 11:56:19 -0300

    instalacao do flake8 e ajustes

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -8,6 +8,7 @@ class TaskCreatorPermission(permissions.BasePermission):
             view.queryset = view.queryset.filter(creator=request.user)
             return True
         return super().has_permission(request, view)
+
     
     def has_object_permission(self, request, view, obj):
         return obj.creator == request.user
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -9,7 +9,8 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ('id', 'title', 'creator', 'description', 
                   'due_date', 'created_at', 'updated_at')
-    
+
+
     def validate_due_date(self, value):
         current_date = date.today()
         if value < current_date:
